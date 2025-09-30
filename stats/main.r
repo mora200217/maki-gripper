dcdf2983d91877fd9b55731f96e825d055f66251
SamuelCruz72 <sacruzs@unal.edu.co>
2025-09-29 22:29:24 -0500

    update R

--- stats/main.r
+++ stats/main.r
@@ -19,9 +19,9 @@ n_mass <- length(mass)
 error <- qnorm(0.975) * sd_mass / sqrt(n)
 ci <- c(mean_mass - error, mean_mass + error)
 
-boxplot(mass, main="Boxplot the masas medidas en muestra", ylabel="Gramos(g)")
-#Major diameter 
+boxplot(mass, main="Boxplot de masas medidas en muestra", ylabel="Gramos(g)")
 
+#Major diameter 
 d_M <- data$D_M..cm.
 mean_d_M <- mean(d_M)
 sd_d_M   <- sd(d_M)
@@ -37,4 +37,22 @@ n_d_M <- length(d_M)
 error_2 <- qnorm(0.975) * sd_d_M / sqrt(n_d_M)
 ci_2 <- c(mean_d_M - error_2, mean_d_M + error_2)
 
-boxplot(d_M, main="Boxplot the masas medidas en muestra", ylabel="Gramos(g)")
+boxplot(d_M, main="Boxplot de diámetros mayores medidos en muestra", ylabel="Diámtro(cm)")
+
+#Minor diameter 
+d_m <- data$d_m..cm.
+mean_d_m <- mean(d_m)
+sd_d_m  <- sd(d_m)
+
+
+hist(d_m, breaks=10, probability=TRUE,
+     col="lightblue", main="Distribución de diámetros mayores",
+     xlab="Masa ()")
+x <- seq(min(d_m), max(d_m), length=100)
+lines(x, dnorm(x, mean=mean_d_M, sd=sd_d_M), col="red", lwd=2)
+
+n_d_M <- length(d_M)
+error_2 <- qnorm(0.975) * sd_d_M / sqrt(n_d_M)
+ci_2 <- c(mean_d_M - error_2, mean_d_M + error_2)
+
+boxplot(d_M, main="Boxplot de diámetros mayores medidos en muestra", ylabel="Diámtro(cm)")
