fbee7e8f22fbdc08a8c1ce9a29015c3787682819
Andrés Morales <amoralesma@unal.edu.co>
2025-09-29 23:00:27 -0500

    [Update] stats final

--- stats/main.r
+++ stats/main.r
@@ -1,34 +1,33 @@
-<<<<<<< HEAD
-# Cargar datos
+# Maki Gripper 
+# ---
+# Tratamiento estadístico de muestras de tomate (n = 30) 
+
 data <- read.csv("dataset-28-09-25.csv", header = TRUE, sep = ",")
 
 head(data)
 
-mass <- data$m..g.
-=======
-datos <- read.csv("G:/Universidad/Trabajos_Mecatronica/9_Semestre/PAI/Tomate.csv") 
-#Mass
+# === MASAS 
+
 mass <- datos$`m..g.` 
 m1 <- mean(x) 
 sd1 <- sd(x)
 hist(mass, breaks = 10, probability = TRUE, col = "lightblue", border = "black", main = "Distribución de masas", xlab = "Masa (g)")  
 curve(dnorm(x, mean = m1, sd = sd1), col = "red", lwd = 2, add = TRUE)
 boxplot(mass,main = "Distribución de masas", ylab = "Masa (g)", xlab = "Muestras")
-#Major diameter
+
+
 d_M <- datos$`D_M..cm.` 
 m2 <- mean(d_M) 
 sd2 <- sd(d_M)
 hist(d_M, breaks = 10, probability = TRUE, col = "lightblue", border = "black", main = "Distribución de diámetros mayores", xlab = "Diámetros Mayores (cm)")  
 curve(dnorm(x, mean = m2, sd = sd2), col = "red", lwd = 2, add = TRUE)
-#Minor diameter
+
 d_m <- datos$`d_m..cm.` 
 m3 <- mean(d_m) 
 sd3 <- sd(d_m)
 hist(d_m, breaks = 10, probability = TRUE, col = "lightblue", border = "black", main = "Distribución de diámetros menores", xlab = "Diámetros Menores (cm)")  
 curve(dnorm(x, mean = m3, sd = sd3), col = "red", lwd = 2, add = TRUE)
 boxplot(d_M, d_m,main = "Distribución de masas", ylab = "Masa (g)", xlab = "Muestras")
->>>>>>> 3cd7c2bde01346fcf8483e8960f5ec1f7316505e
-
 
 mean_mass <- mean(mass)
 sd_mass   <- sd(mass)
@@ -47,4 +46,30 @@ ci
 
 
 boxplot(mass, main="Boxplot the masas medidas en muestra")
+
+
+# Inversa unilateral 
+summary(data)
+
+## Limpieza para masa
+
+# Calcular Q1 y Q3
+mass 
+Q1 <- quantile(mass, 0.25)
+Q3 <- quantile(mass, 0.75)
+IQR <- Q3 - Q1
+
+
+lim_inf <- Q1 - 1.5 * IQR
+lim_sup <- Q3 + 1.5 * IQR
+
+# Filtrar sin atípicos
+mass 
+mass_clean <- mass[mass >= lim_inf & mass <= lim_sup]
+mass_clean
+
+boxplot(list(Original = mass, Limpio = mass_clean))
+
+summary(mass_clean)
+summary(mass)
 
