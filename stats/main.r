f23eb9854f5d5765d4834387342c7ba968a4efe9
SamuelCruz72 <sacruzs@unal.edu.co>
2025-10-25 11:34:28 -0500

    Update_R

--- stats/main.r
+++ stats/main.r
@@ -1,6 +1,6 @@
 # Maki Gripper 
 # ---
-# Tratamiento estadistico de muestras de tomate (n = 30) 
+# Tratamiento estadístico de muestras de tomate (n = 30) 
 
 #Load data
 data <- read.csv("dataset-28-09-25.csv", header = TRUE, sep = ",")
@@ -22,7 +22,6 @@ n_mass <- length(mass)
 error_m <- qnorm(0.975) * sd_mass / sqrt(n_mass)
 ci_mass <- c(mean_mass - error_m, mean_mass + error_m)
 
-boxplot(mass, main="Boxplot de masas medidas en muestra", ylabel="Masa (g)")
 
 #Major diameter 
 d_M <- (data$D_M..cm.)/(2*pi)
@@ -39,8 +38,6 @@ n_d_M <- length(d_M)
 error_d_M <- qnorm(0.975) * sd_d_M / sqrt(n_d_M)
 ci_d_M <- c(mean_d_M - error_d_M, mean_d_M + error_d_M)
 
-boxplot(d_M, main="Boxplot de diámetros mayores medidos en muestra", ylabel="Diámetros (cm)")
-
 #Minor diameter 
 d_m <- (data$d_m..cm.)/(2*pi)
 mean_d_m <- mean(d_m)
@@ -56,8 +53,6 @@ n_d_m <- length(d_m)
 error_d_m <- qnorm(0.975) * sd_d_m / sqrt(n_d_m)
 ci_d_m <- c(mean_d_m - error_d_m, mean_d_m + error_d_m)
 
-boxplot(d_m, main="Boxplot de diámetros menores medidos en muestra", ylabel="Diámetros (cm)")
-
 # Inversa unilateral 
 summary(data)
 
@@ -65,20 +60,64 @@ summary(data)
 
 # Calcular Q1 y Q3
 mass 
-Q1 <- quantile(mass, 0.25)
-Q3 <- quantile(mass, 0.75)
-IQR <- Q3 - Q1
+Q1_mass <- quantile(mass, 0.25)
+Q3_mass <- quantile(mass, 0.75)
+IQR_mass <- Q3_mass - Q1_mass
 
-
-lim_inf <- Q1 - 1.5 * IQR
-lim_sup <- Q3 + 1.5 * IQR
+inf_mass <- Q1_mass - 1.5 * IQR_mass
+sup_mass <- Q3_mass + 1.5 * IQR_mass
 
 # Filtrar sin atípicos
 mass 
-mass_clean <- mass[mass >= lim_inf & mass <= lim_sup]
+mass_clean <- mass[mass >= inf_mass & mass <= sup_mass]
 mass_clean
 
-boxplot(list(Original = mass, Limpio = mass_clean))
+boxplot(main="Boxplot de masas medidas en muestra", ylab="Masa (g)", 
+        list(Original = mass, Limpio = mass_clean))
 
 summary(mass_clean)
 summary(mass)
+
+## Limpieza para diámetro mayor
+
+# Calcular Q1 y Q3
+d_M
+Q1_d_M <- quantile(d_M, 0.25)
+Q3_d_M <- quantile(d_M, 0.75)
+IQR_d_M <- Q3_d_M - Q1_d_M
+
+inf_d_M <- Q1_d_M - 1.5 * IQR_d_M
+sup_d_M <- Q3_d_M + 1.5 * IQR_d_M
+
+# Filtrar sin atípicos
+d_M
+d_M_clean <- d_M[d_M >= inf_d_M & d_M <= sup_d_M]
+d_M_clean
+
+boxplot(main="Boxplot de diámetros mayores medidos en muestra", ylab="Diámetros (cm)", 
+        list(Original = d_M, Limpio = d_M_clean))
+
+summary(d_M_clean)
+summary(d_M)
+
+## Limpieza para diámetro menor
+
+# Calcular Q1 y Q3
+d_m
+Q1_d_m <- quantile(d_m, 0.25)
+Q3_d_m <- quantile(d_m, 0.75)
+IQR_d_m <- Q3_d_m - Q1_d_m
+
+inf_d_m <- Q1_d_m - 1.5 * IQR_d_m
+sup_d_m <- Q3_d_m + 1.5 * IQR_d_m
+
+# Filtrar sin atípicos
+d_m
+d_m_clean <- d_m[d_m >= inf_d_m & d_m <= sup_d_m]
+d_m_clean
+
+boxplot(main="Boxplot de diámetros menores medidos en muestra", ylab="Diámetros (cm)", 
+        list(Original = d_m, Limpio = d_m_clean))
+
+summary(d_m_clean)
+summary(d_m)
