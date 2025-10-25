2d97c6fe14d9d5cb62a54a8179ea98680a22b40c
SamuelCruz72 <sacruzs@unal.edu.co>
2025-10-25 17:39:07 -0500

    Update R

--- stats/main.r
+++ stats/main.r
@@ -11,47 +11,55 @@ head(data)
 mass <- data$m..g.
 mean_mass <- mean(mass)
 sd_mass <- sd(mass)
+n_mass <- length(mass)
+error_m <- qnorm(0.975) * sd_mass / sqrt(n_mass)
+ci_mass <- c(mean_mass - error_m, mean_mass + error_m)
 
 hist(mass, breaks=10, probability=TRUE,
      col="lightblue", main="Distribución de masas",
      xlab="Masa (g)")
 x_mass <- seq(min(mass), max(mass), length=100)
 lines(x_mass, dnorm(x_mass, mean=mean_mass, sd=sd_mass), col="red", lwd=2)
-
-n_mass <- length(mass)
-error_m <- qnorm(0.975) * sd_mass / sqrt(n_mass)
-ci_mass <- c(mean_mass - error_m, mean_mass + error_m)
-
+text(x = max(mass)*0.8, y = 0.012,
+     labels = paste("Media =", round(mean_mass, 2),
+                    "\nDesv =", round(sd_mass, 2)),
+     pos = 4)
 
 #Major diameter 
 d_M <- (data$D_M..cm.)/(2*pi)
 mean_d_M <- mean(d_M)
 sd_d_M   <- sd(d_M)
+n_d_M <- length(d_M)
+error_d_M <- qnorm(0.975) * sd_d_M / sqrt(n_d_M)
+ci_d_M <- c(mean_d_M - error_d_M, mean_d_M + error_d_M)
 
 hist(d_M, breaks=10, probability=TRUE,
      col="lightblue", main="Distribución de diámetros mayores",
      xlab="Diámetros (cm)")
 x_d_M <- seq(min(d_M), max(d_M), length=100)
 lines(x_d_M, dnorm(x_d_M, mean=mean_d_M, sd=sd_d_M), col="red", lwd=2)
-
-n_d_M <- length(d_M)
-error_d_M <- qnorm(0.975) * sd_d_M / sqrt(n_d_M)
-ci_d_M <- c(mean_d_M - error_d_M, mean_d_M + error_d_M)
+text(x = max(d_M)*0.9, y = 0.7,
+     labels = paste("Media =", round(mean_d_M, 2),
+                    "\nDesv =", round(sd_d_M, 2)),
+     pos = 4)
 
 #Minor diameter 
 d_m <- (data$d_m..cm.)/(2*pi)
 mean_d_m <- mean(d_m)
 sd_d_m  <- sd(d_m)
+n_d_m <- length(d_m)
+error_d_m <- qnorm(0.975) * sd_d_m / sqrt(n_d_m)
+ci_d_m <- c(mean_d_m - error_d_m, mean_d_m + error_d_m)
 
 hist(d_m, breaks=10, probability=TRUE,
      col="lightblue", main="Distribución de diámetros menores",
      xlab="Diámetros (cm)")
 x_d_m <- seq(min(d_m), max(d_m), length=100)
 lines(x_d_m, dnorm(x_d_m, mean=mean_d_M, sd=sd_d_M), col="red", lwd=2)
-
-n_d_m <- length(d_m)
-error_d_m <- qnorm(0.975) * sd_d_m / sqrt(n_d_m)
-ci_d_m <- c(mean_d_m - error_d_m, mean_d_m + error_d_m)
+text(x = max(d_m)*0.8, y = 1.5,
+     labels = paste("Media =", round(mean_d_m, 2),
+                    "\nDesv =", round(sd_d_m, 2)),
+     pos = 4)
 
 # Inversa unilateral 
 summary(data)
@@ -72,7 +80,7 @@ mass_clean <- mass[mass >= inf_mass & mass <= sup_mass]
 mass_clean
 
 boxplot(main="Boxplot de masas medidas en muestra", ylab="Masa (g)", 
-        list(Original = mass, Limpio = mass_clean))
+        list(Original = mass, Filtrado = mass_clean))
 
 summary(mass_clean)
 summary(mass)
@@ -93,7 +101,7 @@ d_M_clean <- d_M[d_M >= inf_d_M & d_M <= sup_d_M]
 d_M_clean
 
 boxplot(main="Boxplot de diámetros mayores medidos en muestra", ylab="Diámetros (cm)", 
-        list(Original = d_M, Limpio = d_M_clean))
+        list(Original = d_M, Filtrado = d_M_clean))
 
 summary(d_M_clean)
 summary(d_M)
@@ -114,7 +122,7 @@ d_m_clean <- d_m[d_m >= inf_d_m & d_m <= sup_d_m]
 d_m_clean
 
 boxplot(main="Boxplot de diámetros menores medidos en muestra", ylab="Diámetros (cm)", 
-        list(Original = d_m, Limpio = d_m_clean))
+        list(Original = d_m, Filtrado = d_m_clean))
 
 summary(d_m_clean)
 summary(d_m)
